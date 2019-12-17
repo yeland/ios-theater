ea2f0ec610b0dae806c2b1fde2b706215ea99727
yeland <wlx100715@163.com>
2019-12-17 08:15:02 +0800

    [weilinxiao] feat: init project

---
+++ ios-theater/ios-theater/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-theater
+//
+//  Created by Linxiao Wei on 2019/12/17.
+//  Copyright © 2019 Linxiao Wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
