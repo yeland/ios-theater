4111c317556e6d57252b865f7d684ce17ef4e0d2
yeland <wlx100715@163.com>
2019-12-25 17:16:56 +0800

    [weilinxiao] feat: show rating stars and number for every movie

---
+++ ios-theater/Utils/UILabel+RatingStars.swift
@@ -0,0 +1,54 @@
+//
+//  RatingStars.swift
+//  ios-theater
+//
+//  Created by Linxiao Wei on 2019/12/25.
+//  Copyright © 2019 Linxiao Wei. All rights reserved.
+//
+
+import UIKit
+
+extension UILabel {
+  func setStars(rating: Double, size: CGFloat) -> NSMutableAttributedString {
+    let notEmptyStarsNumber = Int(ceil(rating/2))
+    let notEmptyStars = generateNotEmptyStars(number: notEmptyStarsNumber, rating: rating, size: size)
+    let emptyStars = generateEmptyStars(number: 5 - notEmptyStarsNumber, size: size)
+    let starsString = NSMutableAttributedString(string: "")
+    starsString.append(notEmptyStars)
+    starsString.append(emptyStars)
+    self.attributedText = starsString
+    return starsString
+  }
+  
+  func generateNotEmptyStars(number: Int, rating: Double, size: CGFloat) -> NSMutableAttributedString {
+    let notEmptyStarsString = NSMutableAttributedString(string: "")
+    if number == 0 { return notEmptyStarsString }
+    let halfFillStarString = generateString(iconName: "star.lefthalf.fill", size: size)
+    let fillStarString = generateString(iconName: "star.fill", size: size)
+    for _ in 0..<number-1 {
+      notEmptyStarsString.append(fillStarString)
+    }
+    if Double(number * 2) > rating {
+      notEmptyStarsString.append(halfFillStarString)
+    } else {
+      notEmptyStarsString.append(fillStarString)
+    }
+    return notEmptyStarsString
+  }
+  
+  func generateEmptyStars(number: Int, size: CGFloat) -> NSMutableAttributedString {
+    let emptyStarsString = NSMutableAttributedString(string: "")
+    let starString = generateString(iconName: "star", size: size)
+    for _ in 0..<number {
+      emptyStarsString.append(starString)
+    }
+    return emptyStarsString
+  }
+  
+  func generateString(iconName: String, size: CGFloat) -> NSAttributedString {
+    let attachment = NSTextAttachment()
+    attachment.image = UIImage(systemName: iconName)?.withRenderingMode(.alwaysTemplate)
+    attachment.bounds = CGRect(x: 0, y: -2, width: size, height: size)
+    return NSAttributedString(attachment: attachment)
+  }
+}
--- ios-theater/View/MovieCollectionCell.swift
+++ ios-theater/View/MovieCollectionCell.swift
@@ -11,10 +11,20 @@ import UIKit
 class MovieCollectionCell: UICollectionViewCell {
   @IBOutlet weak var postImage: UIImageView!
   @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var ratingLabel: UILabel!
   
   func configure(movie: Movie) {
     guard let url = URL(string: movie.images.large) else { return }
     self.postImage.setImage(withURL: url)
     self.nameLabel.text = movie.title
+    setupRatingLabel(rating: movie.rating.average)
+  }
+  
+  func setupRatingLabel(rating: Double) {
+    let ratingNumber = NSMutableAttributedString(string: " \(rating)")
+    let ratingString = ratingLabel.setStars(rating: rating, size: 14)
+    ratingString.append(ratingNumber)
+    ratingString.addAttribute(.foregroundColor, value: UIColor.systemYellow, range: NSRange(location: 0, length: 5))
+    ratingLabel.attributedText = ratingString
   }
 }
