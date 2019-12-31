30a2fff595a3339ca89f25f4e5107b5ebf1ae4c2
yeland <wlx100715@163.com>
2019-12-31 18:04:01 +0800

    [weilinxiao] feat: show movie's basic information and implement internationalization

--- ios-theater/Controller/MovieDetailsPage/MovieDetailsViewController.swift
+++ ios-theater/Controller/MovieDetailsPage/MovieDetailsViewController.swift
@@ -9,14 +9,40 @@
 import UIKit
 
 class MovieDetailsViewController: UIViewController {
+  @IBOutlet weak var posterImage: UIImageView!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var originalTitleAndYear: UILabel!
+  @IBOutlet weak var basicInfoLabel: UILabel!
+  
   private var movieId = ""
+  private let movieDetailsViewModel = MovieDetailsViewModel()
   
   override func viewDidLoad() {
     navigationController?.navigationBar.isHidden = false
-    self.title = movieId
+    let title = NSLocalizedString("movie", comment: "")
+    self.title = title
+    fetchData()
   }
   
   func configure(withMovieId movieId: String) {
     self.movieId = movieId
+  }
+  
+  private func fetchData(){
+    movieDetailsViewModel.fetchMovieDetails(movieId: movieId) {
+      guard let movie = self.movieDetailsViewModel.movie else { return }
+      guard let url = URL(string: movie.images.large) else { return }
+      self.posterImage.setImage(withURL: url)
+      self.posterImage.layer.cornerRadius = 5
+      self.titleLabel.text = movie.title
+      self.originalTitleAndYear.text = "\(self.movieDetailsViewModel.showedOriginalTitleAndYear)"
+      let basicInfoString = NSMutableAttributedString(string: self.movieDetailsViewModel.showedBasicInformation)
+      let rightAttachment = NSTextAttachment()
+      rightAttachment.image = UIImage(systemName: "chevron.right")?.withTintColor(.darkGray)
+      rightAttachment.bounds = CGRect(x: 0, y: -1, width: 8, height: 12)
+      let rightString = NSAttributedString(attachment: rightAttachment)
+      basicInfoString.append(rightString)
+      self.basicInfoLabel.attributedText = basicInfoString
+    }
   }
 }
--- ios-theater/ViewModel/MovieDetailsViewModel.swift
+++ ios-theater/ViewModel/MovieDetailsViewModel.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+
+class MovieDetailsViewModel {
+  private let networkClient = NetworkClient()
+  var movie: Movie?
+  
+  func fetchMovieDetails(movieId: String, completion: @escaping () -> Void) {
+    guard let url = URL(string: "https://api.douban.com/v2/movie/subject/\(movieId)?apikey=0df993c66c0c636e29ecbb5344252a4a") else { return }
+    networkClient.request(url: url) { [weak self] data, error in
+      if let data = data {
+        do {
+          self?.movie = try JSONDecoder().decode(Movie.self, from: data)
+          completion()
+        } catch let error {
+          print(error.localizedDescription)
+        }
+      }
+      
+      if let error = error {
+        print(error.localizedDescription)
+      }
+    }
+  }
+  
+  var showedOriginalTitleAndYear: String {
+    let originalTitle: String
+    guard let movie = movie else { return "" }
+    if movie.title != movie.original_title {
+      originalTitle = movie.original_title
+    } else {
+      originalTitle = ""
+    }
+    return "\(originalTitle)(\(movie.year))"
+  }
+  
+  var showedBasicInformation: String {
+    guard let movie = movie, let pubdates = movie.pubdates else { return "" }
+    let pubdate = pubdates[pubdates.count - 1]
+    return "\(movie.countries?[0] ?? "") / \(movie.genres.joined(separator: " ")) / 上映时间：\(pubdate) / 片长: \(movie.durations?[0] ?? "") "
+  }
+}
